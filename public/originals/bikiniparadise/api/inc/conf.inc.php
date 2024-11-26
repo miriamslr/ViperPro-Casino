b9b783d3503af1b569d5cbe02308721571cb5df2
Casino-Scripts.com <heartsofironivsteam@gmail.com>
2024-11-26 03:54:24 +0100

    DL

--- public/originals/bikiniparadise/api/inc/conf.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$res = new stdClass();
-$con = new mysqli("localhost", "root", "", "ggds_casinoprovider");
--- routes/groups/api/profile/profile.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-use App\Http\Controllers\Api\Profile\ProfileController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', [ProfileController::class, 'index']);
-Route::post('/upload-avatar', [ProfileController::class, 'uploadAvatar']);
-Route::post('/updateName', [ProfileController::class, 'updateName']);
